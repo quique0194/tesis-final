8bc7cba35749e8df01bbd4ba59774059549fdb47
Jose Carrillo <quique0194@gmail.com>
2016-12-05 18:27:20 -0500

    Add pillow to dependencies

--- install_everything.sh
+++ install_everything.sh
@@ -7,4 +7,4 @@ sudo apt-get install -y git build-essential python-dev
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 sudo python get-pip.py
 
-sudo pip install numpy pygame theano ipython
+sudo pip install numpy pygame theano ipython pillow
